894467c4665fa482cb2a6082b6bb98f7077d4646
Nafisa-Fairuzia <nafisafairuzia@example.com>
2025-01-12 20:02:35 -0800

    first commit

---
+++ assets/js/slider.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+  $(".autoplay").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+});
